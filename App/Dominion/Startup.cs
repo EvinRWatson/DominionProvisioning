227be364229c39973fea2e34f1cc0591d24d3ac2
Evin Watson <evinwatson@outlook.com>
2021-04-09 13:13:36 -0700

    Using Statements
    Changed them for consistency and ease of use

--- App/Dominion/Startup.cs
+++ App/Dominion/Startup.cs
@@ -1,15 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Dominion.Data;
 using Microsoft.EntityFrameworkCore;
+
+using Dominion.Data;
 
 namespace Dominion
 {
