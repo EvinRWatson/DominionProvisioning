e88ba50a104b7cd58b5885b9cae87270effd8ee4
Evin Watson <evinwatson@outlook.com>
2021-04-16 17:08:21 -0700

    Edit Request Views
    Changed what the views for requests showed as well as changed the navigation in the views to be bootstrap buttons

--- App/Dominion/Models/Users.cs
+++ App/Dominion/Models/Users.cs
@@ -10,10 +10,10 @@ namespace Dominion.Models
         [MaxLength(15, ErrorMessage = "Name can only be 100 characters")]
         public string UserName { get; set; }
 
-        [MaxLength(30, ErrorMessage = "Name can only be 100 characters")]
+        [MaxLength(30, ErrorMessage = "Name can only be 30 characters")]
         public string FirstName { get; set; }
 
-        [MaxLength(30, ErrorMessage = "Name can only be 100 characters")]
+        [MaxLength(30, ErrorMessage = "Name can only be 30 characters")]
         public string LastName { get; set; }
 
         public int OrgID { get; set; }
